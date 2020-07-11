dbab394f984baada09f132030a66946651af97e2
Vincent McGargill <vincentmcgargill@gmail.com>
2020-07-11 03:32:17 -0600

    Got the app in a state where it can check if the answer is right.

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const StartQuizButton = document.getElementById("StartQuizButton");
 const ShowHighScore = document.getElementById("ShowHighScore");
 const SubmitAnswerForm = document.getElementById("SubmitAnswerForm");
 const QuizQuestion = document.getElementById("QuizQuestion");
+const AnswerForm = document.querySelectorAll('input[name="AnswerForm"]');
 const A = document.getElementById("A");
 const LA = document.getElementById("LA");
 const B = document.getElementById("B");
@@ -14,145 +15,103 @@ const LD = document.getElementById("LD");
 const SubmitAnswerButton = document.getElementById("SubmitAnswerButton");
 const CancelQuizButton = document.getElementById("CancelQuizButton");
 const ShowCorrectAnswer = document.getElementById("ShowCorrectAnswer");
+const NextQuestionButton = document.getElementById("NextQuestionButton");
+const EndQuizButton = document.getElementById("EndQuizButton");
+
+var UserHighScore = 0;
+
+let questions = [
+    {
+        question: "What is my favorite food?",
+        A: "Pizza",
+        B: "Steak",
+        C: "Hamburger",
+        D: "Salad",
+        answer: "A" 
+    },
+    {
+        question: "What is JavaScript?",
+        A: "A hyper text transfer",
+        B: "A programming language",
+        C: "A computer application",
+        D: "An addressbook for the internet",
+        answer: "B"
+    },
+    {
+        question: "How old are you?",
+        A: "1",
+        B: "2",
+        C: "3",
+        D: "4",
+        answer: "D"
+    }
+]
 
 
+// Start Quiz
 function StartQuiz() {
-    // Hide the Start Quiz Button and show the Submit Answer Form
     StartQuizButton.classList.add("hidden");
     ShowHighScore.classList.add("hidden");
-    SubmitAnswerForm.classList.remove("hidden");
+    GenerateQuestion()
 }
 
-function SubmitAnswer() {
-    // 
+
+function GenerateQuestion() {
+    SubmitAnswerForm.classList.remove("hidden");
+    for (var qIndex = 0; qIndex < questions.length; qIndex++) {
+        QuizQuestion.innerHTML = questions[qIndex].question;
+        LA.innerHTML = questions[qIndex].A;
+        LB.innerHTML = questions[qIndex].B;
+        LC.innerHTML = questions[qIndex].C;
+        LD.innerHTML = questions[qIndex].D;
+        // alert(questions[qIndex].answer)
+        
+        let CorrectAnswer = questions[qIndex].answer
+        let UserAnser;
+
+        SubmitAnswerButton.onclick = function CheckAnswer() {
+            for (var checkedAnswer of AnswerForm) {
+                if (checkedAnswer.checked) {
+                    UserAnser = checkedAnswer.value;
+                    break;
+                }
+            }
+
+            SubmitAnswerForm.classList.add("hidden");
+            ShowCorrectAnswer.classList.remove("hidden");
+            NextQuestionButton.classList.remove("hidden");
+            NextQuestionButton.classList.add("btn")
+
+
+            if (UserAnser == CorrectAnswer) {
+                ShowCorrectAnswer.classList.add("AnswerCorrect");
+                ShowCorrectAnswer.innerText = "That is correct! The answer is " + UserAnser
+            } else {
+                ShowCorrectAnswer.classList.add("AnswerIncorrect");
+                ShowCorrectAnswer.innerText = "That is incorrect! The answer is " + CorrectAnswer
+            }
+        }
+    } 
+}
+
+function NextQuestion() {
+    ShowCorrectAnswer.classList.add("hidden");
+    NextQuestionButton.classList.add("hidden");
+
+    // Reminder to remove this later.
+    EndQuiz()
+}
+
+function CancelQuiz() {
     SubmitAnswerForm.classList.add("hidden");
+}
+
+// End Quiz
+function EndQuiz() {
     StartQuizButton.classList.remove("hidden");
     ShowHighScore.classList.remove("hidden");
+    EndQuizButton.classList.remove("hidden");
+    EndQuizButton.classList.add("btn")
+    ShowCorrectAnswer.classList.add("hidden");
+    NextQuestionButton.classList.add("hidden");
 }
-
-function ShowAnswer() {
-
-}
-
-
-
-/// GENERATE HEADER ///
-// var header = document.getElementById("quizHeader");
-// header.setAttribute("style", "color: white; background-color: blue; text-align: center; padding: 30px;");
-// header.textContent = "Code Quiz";
-
-// /// GENERATE FOOTER ///
-// var footer = document.getElementById("quizFooter");
-// footer.setAttribute("style", "color: white; background-color: blue; text-align: center; padding: 30px");
-// footer.textContent = "© 2020 Vincent McGargill";
-
-
-// /// GENERATE QUESTION FUNCTION ///
-// function GenerateQuestions() {
-    
-//     // Update attributes for quizFormDiv
-//     QuizFormDiv.setAttribute("class", "quizForm");
-    
-//     // Generate Question Header
-//     var QuestionHeader = document.createElement("h1");
-//     QuestionHeader.textContent = "What is my favorite food?";
-//     QuizFormDiv.appendChild(QuestionHeader);
-    
-//     // Generate Radio Asnwer Form
-//     var answerForm = document.createElement("form");
-//     answerForm.setAttribute("id", "answerForm");
-//     answerForm.setAttribute("action", "submitAnswer");
-//     QuizFormDiv.appendChild(answerForm);
-    
-//     // Create First Answer Choice
-//     var answer1 = document.createElement("input");
-//     answer1.setAttribute("type", "radio");
-//     answer1.setAttribute("name", "food");
-//     answer1.setAttribute("value", "pizza");
-//     answer1.setAttribute("id", "a1");
-//     var answer1label = document.createElement("label")
-//     answer1label.setAttribute("for", "a1")
-//     answer1label.innerHTML = "Pizza"
-//     answerForm.appendChild(answer1);
-//     answerForm.appendChild(answer1label);
-//     lineBreak = document.createElement("br");
-//     answerForm.appendChild(lineBreak);
-    
-//     // Create Second Answer Choice
-//     var answer2 = document.createElement("input");
-//     answer2.setAttribute("type", "radio");
-//     answer2.setAttribute("name", "food");
-//     answer2.setAttribute("value", "steak");
-//     answer2.setAttribute("id", "a2");
-//     var answer2label = document.createElement("label")
-//     answer2label.setAttribute("for", "a2")
-//     answer2label.innerHTML = "Steak"
-//     answerForm.appendChild(answer2);
-//     answerForm.appendChild(answer2label);
-//     lineBreak = document.createElement("br");
-//     answerForm.appendChild(lineBreak);
-    
-//     // Create Third Answer Choice
-//     var answer3 = document.createElement("input");
-//     answer3.setAttribute("type", "radio");
-//     answer3.setAttribute("name", "food");
-//     answer3.setAttribute("value", "hamburger");
-//     answer3.setAttribute("id", "a3");
-//     var answer3label = document.createElement("label")
-//     answer3label.setAttribute("for", "a3")
-//     answer3label.innerHTML = "Hamburger"
-//     answerForm.appendChild(answer3);
-//     answerForm.appendChild(answer3label);
-//     lineBreak = document.createElement("br");
-//     answerForm.appendChild(lineBreak);
-    
-//     // Create Fourth Answer Choice
-//     var answer4 = document.createElement("input");
-//     answer4.setAttribute("type", "radio");
-//     answer4.setAttribute("name", "food");
-//     answer4.setAttribute("value", "salad");
-//     answer4.setAttribute("id", "a4");
-//     var answer4label = document.createElement("label")
-//     answer4label.setAttribute("for", "a4")
-//     answer4label.innerHTML = "Salad"
-//     answerForm.appendChild(answer4);
-//     answerForm.appendChild(answer4label);
-//     lineBreak = document.createElement("br");
-//     answerForm.appendChild(lineBreak);
-    
-//     function test() {
-//         alert("Hello world!")
-//     }
-    
-//     // Create Answer Button
-//     const SubmitAnswerbutton = document.createElement("button");
-//     SubmitAnswerbutton.setAttribute("class", "btn");
-//     SubmitAnswerbutton.setAttribute("id", "submitAnswerBtn");
-//     SubmitAnswerbutton.textContent = "Submit Answer";
-//     answerForm.appendChild(SubmitAnswerbutton);
-//     var submitAnswerBtn = getElementById("submitAnswerBtn");
-//     submitAnswerBtn.addEventListener('click', test);
-
-    
-    
-//     // Create Cancel Button
-//     var cancelGamebtn = document.createElement("button");
-//     cancelGamebtn.setAttribute("class", "btn");
-//     cancelGamebtn.setAttribute("id", "cancelGameBtn");
-//     cancelGamebtn.textContent = "Cancel Game";
-//     answerForm.appendChild(cancelGamebtn);            
-// }
-
-
-// /// START QUIZ ///
-// function StartQuiz() {
-    
-//     // Hide the start button
-//     StartQuizButton.style.display = "none";
-    
-//     // Create for loop that selects a pool of random questions and answers.
-//     // TODO: This has not been done yet!
-    
-//     // Run generate question formula
-//     GenerateQuestions()
-// }
